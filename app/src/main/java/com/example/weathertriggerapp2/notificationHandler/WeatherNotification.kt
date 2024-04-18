0fb894af0cce8f75d4f31310f2e03d570eac3ace
julia <>
2024-04-18 21:17:19 +0100

    Added battery constraint to weather notification

--- app/src/main/java/com/example/weathertriggerapp2/notificationHandler/WeatherNotification.kt
+++ app/src/main/java/com/example/weathertriggerapp2/notificationHandler/WeatherNotification.kt
@@ -4,13 +4,16 @@ import android.annotation.SuppressLint
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Context.BATTERY_SERVICE
 import android.content.Intent
+import android.os.BatteryManager
 import com.example.weathertriggerapp2.broadcast.Notification
 import java.util.Calendar
 
 
 // https://www.geeksforgeeks.org/schedule-notifications-in-android/
 // https://stackoverflow.com/questions/14980899/how-to-set-time-to-24-hour-format-in-calendar
+// https://www.geeksforgeeks.org/get-battery-level-in-android-using-jetpack-compose/
 
 /**
  * Class for scheduling weather update notification
@@ -30,6 +33,8 @@ class WeatherNotification(val context: Context) {
         )
 
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val batteryManager = context.getSystemService(BATTERY_SERVICE) as BatteryManager
+        val batteryCharge: Int = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
 
         val calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, 9)
@@ -37,10 +42,13 @@ class WeatherNotification(val context: Context) {
 
         val time = calendar.timeInMillis
 
-        alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            time,
-            pendingIntent
-        )
+        // Only schedule of battery charge is over 35% as GPS tends to kill battery
+        if(batteryCharge >= 35) {
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                time,
+                pendingIntent
+            )
+        }
     }
 }
